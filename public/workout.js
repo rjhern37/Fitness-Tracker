c17f8cce7ca9eb9266b5b2924e50bb287985b9d5
Ray H <rjhern37@neiu.edu>
2020-06-22 18:35:27 -0500

    fixed the totalduration undefined error

--- public/workout.js
+++ public/workout.js
@@ -6,9 +6,11 @@ async function initWorkout() {
       .querySelector("a[href='/exercise?']")
       .setAttribute("href", `/exercise?id=${lastWorkout._id}`);
 
+      let totalDuration = lastWorkout.exercises.reduce((total, exercise) => total + exercise.duration, 0);
+
     const workoutSummary = {
       date: formatDate(lastWorkout.day),
-      totalDuration: lastWorkout.totalDuration,
+      totalDuration,
       numExercises: lastWorkout.exercises.length,
       ...tallyExercises(lastWorkout.exercises)
     };
